202de2e63772ab72454322709050ce729f9166e2
jaeyeong04 <jeayoung05050@naver.com>
2025-12-09 13:19:58 +0900

    feat: state update after keyboard shortcut added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import "./App.css";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   const handleKeyDown = useCallback((event: KeyboardEvent) => {
     if (event.ctrlKey && event.key === "s") {
       event.preventDefault(); //브라우저의 기본 동작 방지
@@ -10,6 +11,10 @@ function App() {
     if (event.shiftKey && event.key === "J") {
       event.preventDefault(); //브라우저의 기본 동작 방지
       alert("Shift + J 단축키가 눌렸습니다!");
+    }
+    if (event.ctrlKey && event.key === "ArrowUp") {
+      event.preventDefault();
+      setCount((prevCount) => prevCount + 1);
     }
   }, []);
   //키보드 이벤트 리스너 등록 및 해제
@@ -30,6 +35,8 @@ function App() {
         아닙니다ㅎㅎ
       </p>
       <p>나만의 단축키를 테스트 해볼까요? Shift + J를 눌러봅시다!</p>
+      <p>그리고 Ctrl + ArrowUp 도 눌러보세요!</p>
+      <p>현재 카운트: {count}</p>
     </div>
   );
 }
